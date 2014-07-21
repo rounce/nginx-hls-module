d5f56a8b6ccd42df5560d98e48e8ad19d0d30c3a
Isaev Ivan <1@kraslan.ru>
2014-07-21 21:45:53 +0800

    remove krasview code

--- src/view_count.c
+++ src/view_count.c
@@ -17,24 +17,6 @@
 #include "mp4_io.h"
 
 extern void view_count(struct mp4_context_t *mp4_context, char *filename, char *hash, char action[50]) {
-  if(hash == NULL) return;
-  char str[256];
-  char file[256];
-  strcpy(file, filename);
-  char *fkey = strrchr(file, '/');
-  *fkey = 0;
-  fkey++;
-  char *key = strrchr(file, '/') + 1;
-
-  sprintf(str, "http://p.krasview.ru:9000/?action=%s&key=%s&hash=%s", action, key, hash);
-  MP4_INFO("uri: %s", str);
-  CURL *curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, str);
-    curl_easy_perform(curl);
-
-    curl_easy_cleanup(curl);
-  }
 }
 
 // End Of File
